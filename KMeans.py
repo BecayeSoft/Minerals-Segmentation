24f09492ec9c99800b1d069175ab805ba81aedf0
Becaye <baldebecaye10@gmail.com>
2023-03-11 16:30:33 -0500

    Save the mean of each color channel into a Dataframe

--- KMeans.py
+++ KMeans.py
@@ -1,53 +1,58 @@
-import cv2 as cv
+"""
+ K-Means Clustering
+"""
 
-# ---------------------
-#  K-Means Clustering
 
 import numpy as np
-from ImageHelper import load_images, show
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import cv2 as cv
 from skimage.segmentation import slic
+
+from ImageHelper import load_images, show
+
 
 # Load the image
 images = load_images()
 img = images[1]
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-show(img, 'Original')
+show(img, 'Original Image')
 
 # Darken the image to remove noisy shades
 M = np.ones(img.shape, dtype="uint8") * 30
-darker_img = cv.subtract(img, M)
-show(darker_img, 'Darker Image')
-
-# CLeaning up the image
-# kernel = np.ones((7,7), np.uint8)
-# eroded = cv.erode(img, kernel, iterations=1)
-# dilated = cv.dilate(eroded, kernel, iterations=1)
-#
-# show(img, 'Original')
-# show(eroded, 'Eroded')
-# show(dilated, 'Dilated')
-# show(img * dilated, 'Masked Image')
-
-
-
+darkened_img = cv.subtract(img, M)
+show(darkened_img, 'Darkened Image')
 
 # Apply the SLIC algorithm to segment the image
-# segments = slic(img, n_segments=100, compactness=10)
-segments = slic(light_image, n_segments=36, compactness=70)
-# segments = slic(img)
+segments = slic(darkened_img, n_segments=36, compactness=70, max_size_factor=3)
 
 minerals = []
+channels_means = []
 # Loop through each labeled region and extract the corresponding object from the original image
 for label in np.unique(segments):
     mask = segments == label
     extracted_object = np.zeros(img.shape, dtype="uint8")
     extracted_object[mask] = img[mask]
 
-    show(extracted_object)
+    # show(extracted_object)
     minerals.append(extracted_object)
 
-# show(img)
-# show(minerals[1])
+    b, g, r = cv.split(extracted_object)
+    blue_mean = np.mean(b)
+    green_mean = np.mean(g)
+    red_means = np.mean(r)
+    channels_means.append([blue_mean, green_mean, red_means])
 
-grain = minerals[20]
-b, g, r = cv.split(grain)
+# Save mean of each channel into a Dataframe
+df_channels = pd.DataFrame(channels_means,
+                   columns=['Blue Mean', 'Green Mean', 'Red Mean'])
+
+# Plot the results
+for i in range(0, 36):
+    plt.subplot(6, 6, i + 1)
+    plt.imshow(minerals[i])
+    plt.axis('off')
+
+plt.title('Extracted minerals')
+plt.show()
